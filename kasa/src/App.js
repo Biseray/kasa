763ff95d4e2917bd19853fc99793d48599d9a684
Biseray <allan.biseray@outlook.fr>
2023-05-05 15:44:31 +0200

    correctionRoute

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -10,7 +10,7 @@ import Error404 from "./pages/error404/index";
 import BaseLayout from "./components/BaseLayout";
 
 const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/kasa" element={ <BaseLayout/> } errorElement={<Error404 />}> 
+    <Route path="/kasa/" element={ <BaseLayout/> } errorElement={<Error404 />}> 
         <Route index element={<Home />} loader={locationsLoader} />
         <Route path="/kasa/fichedetail/:id" element={<FicheDetail />} loader={locationLoader} />
         <Route path="/kasa/apropos" element={<Apropos/>} loader={kasaInfo}  />
